e366d3ad8f6a2c6f3be4baefb110e59caff26008
sonchai058 <sonchai058@gmail.com>
2022-01-17 21:19:54 +0700

    d
    d

--- application/modules/aboutus/views/front-end/aboutus.php
+++ application/modules/aboutus/views/front-end/aboutus.php
@@ -6,23 +6,23 @@
 			<div class="wrapper-about-title">
 				<h1>เกี่ยวกับเรา</h1>
 				<div class="about-desc">
-					<h4>ยินดีต้อนรับสู่เว็บไซต์ Luckydoor</h4>
+					<h4>ยินดีต้อนรับสู่เว็บไซต์ Tanisha</h4>
 				</div>
 			</div>
 			<div class="small-12 columns">
 				<br>
-				<h4 class="text-bk"><bold>Lucky Vision</bold></h4>
+				<h4 class="text-bk"><bold>Tanisha Vision</bold></h4>
 				<div class="_dotranslate">
 					<p>ทรัพยากรธรรมชาติบนโลกนี้มีอยู่อย่างจำกัด แต่ความต้องการของมนุษย์มีไม่จำกัดนั้นเป็นเรื่องที่สวนทางกันเสมอ เพราะฉะนั้นทุกสิ่งทุกอย่างที่มีอยู่บนโลกนี้ มนุษย์จะต้องใช้อย่างประหยัด คุ้มค่าที่สุด และเกิดประโยชน์สูงสุดในศตวรรษต่อไป</p>
 					<br>
-					<p>ด้วยเหตุนี้ จึงเป็นที่มาของนวัตกรรมการผลิตสินค้าในนามว่า <bold>"LUCKY"</bold> ที่เน้นใช้อย่างคุ้มค่าและมีรสนิยมสินค้าที่ผลิตออกมาจะตอบสนองความต้องการของผู้สร้างและผู้อยู่เป็นหลัก กล่าวคือ สินค้าจะต้องแก้ปัญหาให้กับวิศวกรและผู้ออกแบบที่ประสบปัญหาอยู่ในปัจจุบัน ในขณะเดียวกันจะต้องมีรูปแบบที่ดีราคาที่แข่งขันได้ เพื่อใช้ในการทำตลาดของผู้พัฒนาอสังหาริมทรัพย์เป็นต้น</p>
+					<p>ด้วยเหตุนี้ จึงเป็นที่มาของนวัตกรรมการผลิตสินค้าในนามว่า <bold>"Tanisha"</bold> ที่เน้นใช้อย่างคุ้มค่าและมีรสนิยมสินค้าที่ผลิตออกมาจะตอบสนองความต้องการของผู้สร้างและผู้อยู่เป็นหลัก กล่าวคือ สินค้าจะต้องแก้ปัญหาให้กับวิศวกรและผู้ออกแบบที่ประสบปัญหาอยู่ในปัจจุบัน ในขณะเดียวกันจะต้องมีรูปแบบที่ดีราคาที่แข่งขันได้ เพื่อใช้ในการทำตลาดของผู้พัฒนาอสังหาริมทรัพย์เป็นต้น</p>
 					<br>
-					<p>ดังนั้นเราจึงกล้ารับประกันผลิตภัณฑ์ที่ผลิตภายใต้แบรนด์ <bold>"LUCKY"</bold> ว่ามีทั้งคุณภาพดีไซน์และราคาที่แข่งขันได้</p>
+					<p>ดังนั้นเราจึงกล้ารับประกันผลิตภัณฑ์ที่ผลิตภายใต้แบรนด์ <bold>"Tanisha"</bold> ว่ามีทั้งคุณภาพดีไซน์และราคาที่แข่งขันได้</p>
 				</div>
 				<div class="_notranslate notranslate">
 					<p>The finite resources available on earth are insufficient while human wants and needs are unlimited. This is always the big controversy. To enter the new century, human therefore needs to use every resource in the most economic way to make sure they’re spent beneficially and worthy.</p>
 					<br>
-					<p>Realizing so, we provide new innovation of productions under the brand name “Lucky: The brand emphasizes on using up the resources economically, worthy and tastefully. All the products are preeminently produced to fit the needs of the constructors and the residences. It can be said that the products aim to help the designers and engineers who are facing the problems in the present time. Additionally, the products are built in perfect form with competitive price to make sure the real estate developers can use the products in marketing. We dare to guarantee that our products under the brand name “Lucky” provide not only quality and design, but also competitive price.</p>
+					<p>Realizing so, we provide new innovation of productions under the brand name “Tanisha: The brand emphasizes on using up the resources economically, worthy and tastefully. All the products are preeminently produced to fit the needs of the constructors and the residences. It can be said that the products aim to help the designers and engineers who are facing the problems in the present time. Additionally, the products are built in perfect form with competitive price to make sure the real estate developers can use the products in marketing. We dare to guarantee that our products under the brand name “Tanisha” provide not only quality and design, but also competitive price.</p>
 				</div>
 				<br>
 				<h4 class="text-bk"><bold>ประสบการณ์การช้อปปิ้งที่สะดวกสบายบนเว็บไซต์</bold></h4>
@@ -32,7 +32,7 @@
 				<p>เข้าถึงฐานลูกค้าผ่านระบบที่ครอบคลุมทั้งกระบวนการขาย</p>
 				<br>
 				<h4 class="text-bk"><bold>ฝ่ายบริการลูกค้า</bold></h4>
-				<p>หากท่านมีคำถามหรือข้อเสนอแนะใดๆ โปรดติดต่อฝ่ายบริการลูกค้าของ Luckydoor <a href="<?php echo base_url('contactus'); ?>" class="link">คลิกที่นี่</a> ทางเจ้าหน้าที่จะรีบดำเนินการตรวจสอบ และติดต่อกลับท่านภายใน 24 ชม.</p>
+				<p>หากท่านมีคำถามหรือข้อเสนอแนะใดๆ โปรดติดต่อฝ่ายบริการลูกค้าของ Tanisha <a href="<?php echo base_url('contactus'); ?>" class="link">คลิกที่นี่</a> ทางเจ้าหน้าที่จะรีบดำเนินการตรวจสอบ และติดต่อกลับท่านภายใน 24 ชม.</p>
 			</div>
 		</div>
 	</section>
